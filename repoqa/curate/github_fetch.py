bd67f9385f9f5ddc413a26aee99d83ccfdfa4540
ganler <jaway.liu@gmail.com>
2024-03-07 11:47:30 -0600

    feat(cherrypick): python

--- repoqa/curate/github_fetch.py
+++ repoqa/curate/github_fetch.py
@@ -2,10 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import argparse
 import json
 import os
-from dataclasses import dataclass, field
 from datetime import datetime
 from typing import TypedDict
 from zoneinfo import ZoneInfo
@@ -28,49 +26,24 @@ class GitHubDocument(GitHubRepoMeta):
     content: str
 
 
-@dataclass(frozen=True)
-class GitHubFetchingArgs:
-    query: str = field(
-        default="language:python stars:>=1000 license:mit license:apache-2.0 created:>=2023-05-09",
-        metadata={
-            "help": "Rule to search for repositories; check https://docs.github.com/en/search-github/searching-on-github/searching-for-repositories"
-        },
-    )
-
-
 lang2suffix = {
     "python": [".py"],
-    "javascript": [".js"],
     "go": [".go"],
     "c++": [".cpp", ".hpp", ".cc", ".hh", ".cxx", ".hxx", ".c", ".h"],
     "java": [".java"],
     "typescript": [".ts"],
-    "c": [".c", ".h"],
-    "c#": [".cs"],
     "php": [".php"],
     "rust": [".rs"],
 }
 
 
-def main(language: str = "python", stars: int = 10):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--repo-size-interval",
-        type=int,
-        default=10,
-        help="Repo size interval to diversify",
-    )
-    parser.add_argument(
-        "--commit-count", type=int, default=100, help="New commit count to filter repos"
-    )
-    parser.add_argument(
-        "--new-commit-since",
-        type=str,
-        default="2023-09-01",
-        help="Minimum new commit date",
-    )
-    args = parser.parse_args()
-
+def main(
+    language: str = "python",
+    stars: int = 100,
+    minimal_new_commits: int = 50,
+    new_commit_since: str = "2024-01-01",
+    minimal_lang_bytes: int = 1024 * 64,  # 64k ideally
+):
     token = os.getenv("GITHUB_TOKEN")
     assert token is not None, "Make a token at https://github.com/settings/tokens"
     auth = Auth.Token(token)
@@ -80,22 +53,21 @@ def main(language: str = "python", stars: int = 10):
     query.append(f"language:{language}")
     query.append(f"stars:>={stars}")
     query.append("license:mit license:apache-2.0")
-    query.append("pushed:>=2023-09-01")
-    # 50KB to 10MB
-    query.append("size:50..10000")
+    query.append(f"pushed:>={new_commit_since}")
+    # 128KB to 32MB
+    query.append("size:128..32000")
+    query.append("sort:stars")
 
     # compile query
     query = " ".join(query)
+    print(f"{query=}")
     g = Github(auth=auth, per_page=100)
 
     lang_suffix = lang2suffix[language]
 
-    cf_sizes = []
-
     with open(f"{language}-{datetime.now().isoformat()}.jsonl", "w") as f_out:
         repos = g.search_repositories(query)
-        print("Total count", repos.totalCount)
-        # TODO: apply dependency analysis
+        print("Found ", repos.totalCount, "repositories for", language)
         for repo in tqdm(repos, total=repos.totalCount):
             # filter at least 100 commits have been made since 2023 Q4 (>= 2023-09-01).
             commits = repo.get_commits()
@@ -104,15 +76,19 @@ def main(language: str = "python", stars: int = 10):
                     True
                     for commit in commits
                     if commit.last_modified_datetime
-                    >= datetime.strptime(args.new_commit_since, "%Y-%m-%d").replace(
+                    >= datetime.strptime(new_commit_since, "%Y-%m-%d").replace(
                         tzinfo=ZoneInfo("UTC")
                     )
                 )
-            ) < args.commit_count:
+            ) < minimal_new_commits:
+                print(
+                    f"Skip {repo.html_url} for have less than {minimal_new_commits} after {new_commit_since} (only {count_} commits)"
+                )
                 continue
 
-            # filter repos diversified over the size, every interval
+            # filter repos that is large enough
             git_tree = repo.get_git_tree(repo.default_branch, recursive=True)
+
             tree_iter = list(
                 filter(
                     lambda item: item.type == "blob"
@@ -120,11 +96,23 @@ def main(language: str = "python", stars: int = 10):
                     tqdm(git_tree.tree, leave=False),
                 )
             )
-            code_file_size = int(sum(item.size / 1024 for item in tree_iter))
-            if code_file_size // args.repo_size_interval in cf_sizes:
-                continue
-            cf_sizes.append(code_file_size // args.repo_size_interval)
 
+            code_file_size = int(sum(item.size for item in tree_iter))
+            if code_file_size < minimal_lang_bytes:
+                print(
+                    f"Skip {repo.html_url} for have less than {minimal_lang_bytes} bytes source file after {new_commit_since} (only {code_file_size} bytes)"
+                )
+                continue
+
+            schema = dict(
+                repo_name=repo.name,
+                repo_size=code_file_size,
+                repo_owner=repo.owner.login,
+                repo_url=repo.html_url,
+                commit_sha=git_tree.sha,
+                last_modified_time=git_tree.last_modified_datetime,
+                content={},
+            )
             for item in tree_iter:
                 # Fetch the content for each Python file
                 content = repo.get_contents(item.path)
@@ -132,17 +120,9 @@ def main(language: str = "python", stars: int = 10):
                 if content.encoding != "base64":
                     continue
                 file_content = content.decoded_content.decode("utf-8")
-                timestamp = datetime.now().isoformat()
-                data = GitHubDocument(
-                    timestamp=timestamp,
-                    repo_name=repo.name,
-                    repo_owner=repo.owner.login,
-                    repo_size=code_file_size,
-                    commit_sha=git_tree.sha,
-                    path=item.path,
-                    content=file_content,
-                )
-                f_out.write(json.dumps(data) + "\n")
+                schema["content"][item.path] = file_content
+            f_out.write(json.dumps(schema) + "\n")
+            f_out.flush()
 
 
 if __name__ == "__main__":
---
+++ scripts/cherrypick/README.md
@@ -0,0 +1,40 @@
+# GitHub Repository Curation
+
+## Step 1
+
+### PLs
+
+```
+python
+go
+c++
+java
+typescript
+php
+rust
+```
+
+### GitHub searching
+
+Search at: https://github.com/search?
+
+```
+language:{LANGUAGE} stars:>=100 license:mit license:apache-2.0 pushed:>=2024-01-01 size:128..32000 sort:stars
+```
+
+### Find repositories by package rankings
+
+Can also look at package ranking websites such as https://pypistats.org/top
+
+## Step 2
+
+Select 10 repositories based on the following quality criteria:
+1. Uses unit tests and CI/CD
+2. Is a package
+3. Diversify over topics (developer tool, machine learning, etc)
+4. Has enough code (hopefully 10+ files)
+5. Better be pure in one language
+
+## Step 3
+
+Follow the `lists.json` to fill the repositories.
