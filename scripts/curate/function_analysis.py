d2b0b49c8880b3a10f340ca3af6d518dddc07227
vdaita <vdaita@gmail.com>
2024-04-02 14:09:02 -0500

    Debugged find_function_definitions and tested with poetry

--- scripts/curate/function_analysis.py
+++ scripts/curate/function_analysis.py
@@ -1,9 +1,20 @@
 import os
 from tree_sitter import Parser
 from tree_sitter_languages import get_language
-from utility import lang2suffix
 from tempfile import TemporaryDirectory
 from git import Repo
+import json
+
+ts_lang2suffix = { # Changes c++ to cpp
+    "python": [".py"],
+    "go": [".go"],
+    "cpp": [".cpp", ".hpp", ".cc", ".hh", ".cxx", ".hxx", ".c", ".h"],
+    "java": [".java"],
+    "typescript": [".ts"],
+    "php": [".php"],
+    "rust": [".rs"],
+}
+
 
 def find_function_definitions(repo_path):
     # Create a parser
@@ -11,9 +22,11 @@ def find_function_definitions(repo_path):
 
     # Supported languages and file extensions
     extension_map = {}
-    for language in lang2suffix:
-        for suffix in lang2suffix[language]:
+    for language in ts_lang2suffix:
+        for suffix in ts_lang2suffix[language]:
             extension_map[suffix] = get_language(language)
+
+    print(extension_map)
 
     extracted_functions = []
 
@@ -23,15 +36,16 @@ def find_function_definitions(repo_path):
             _, file_extension = os.path.splitext(file)
 
             if file_extension in extension_map:
-                parser.set_language(extension_map[file_extension])
-
+                language = extension_map[file_extension]
+                parser.set_language(language)
+                
                 with open(file_path, 'r') as f:
                     code = f.read()
                     tree = parser.parse(bytes(code, 'utf8'))
 
                     query = language.query("""
                         (function_definition
-                            name: (identifier) @function_name
+                        name: (identifier) @function.def
                         )
                     """)
 
@@ -44,15 +58,10 @@ def find_function_definitions(repo_path):
                             "file_path": file_path,
                             "line_number": line_number
                         })
-
-def get_functions_from_repo(repo_url, entry_dir):
-    with TemporaryDirectory() as tmpdir:
-        Repo.clone_from(repo_url, tmpdir)
-        return find_function_definitions(os.path.join(tmpdir, entry_dir))
-        
-
-
-# Specify the path to your repository
-repo_path = '/path/to/your/repo'
-
-find_function_definitions(repo_path)
+    return extracted_functions
+    
+if __name__ == "__main__":
+    print("Testing function analysis: ")
+    print(json.dumps(find_function_definitions(
+        "./../../repo/poetry/src/poetry/"
+    ), indent=4))
