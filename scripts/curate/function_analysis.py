2ea5467d3a36ed9e3a73269e8419dcd061036c92
V Daita <vdaita@gmail.com>
2024-04-09 02:50:48 -0500

    Function extraction (#9)
    * Starting Function Extraction implementation
    
    * Debugged find_function_definitions and tested with poetry
    
    * Adding full line ranges from function definition to end of block, more accurate formatting
    
    * fix: pre-commit
    
    * feat: tested treesitter support for py, java, ts, rust, cpp
    
    ---------
    
    Co-authored-by: ganler <jaway.liu@gmail.com>

---
+++ scripts/curate/function_analysis.py
@@ -0,0 +1,75 @@
+# SPDX-FileCopyrightText: (c) 2024 EvalPlus Team
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+
+from tqdm import tqdm
+from tree_sitter_languages import get_language, get_parser
+
+FUNCTION_QUERY = {
+    "python": "(function_definition name: (_)) @fdef",
+    "java": "(method_declaration name: (_)) @fdef",
+    "typescript": "(function_declaration name: (_)) @fdef",
+    "rust": "(function_item name: (_)) @fdef",
+    "cpp": "(function_definition declarator: (function_declarator declarator: (identifier))) @fdef",
+}
+
+_default_name_parser = lambda node: node.child_by_field_name("name").text.decode()
+_cpp_name_parser = (
+    lambda node: node.child_by_field_name("declarator")
+    .child_by_field_name("declarator")
+    .text.decode()
+)
+
+
+# Annotate an incomplete repoqa dataset with function and class information
+def main(dataset: str, overwrite_analysis: bool = False):
+    assert dataset.endswith(".json"), "Dataset must be a JSON file, check README"
+    with open(dataset, "r") as f:
+        lists = json.load(f)
+
+    for lang, repos in lists.items():
+        assert (
+            lang in FUNCTION_QUERY
+        ), f"Unsupported language: {lang} -- supported: {FUNCTION_QUERY.keys()}"
+
+        fn_query_text = FUNCTION_QUERY[lang]
+        print(f"🔥 Querying {lang} functions with `{fn_query_text}`...")
+
+        parser = get_parser(lang)
+        fn_query = get_language(lang).query(fn_query_text)
+        fn_name_parser = _cpp_name_parser if lang == "cpp" else _default_name_parser
+
+        for repo in tqdm(repos):
+            # skip if the repo already has function information
+            if not overwrite_analysis and repo.get("functions"):
+                continue
+
+            functions = {}  # path to a list of functions
+            for path, code in repo["content"].items():
+                tree = parser.parse(bytes(code, "utf8"))
+
+                extracted_functions = []
+                for capture in fn_query.captures(tree.root_node):
+                    node, _ = capture
+                    print(fn_name_parser(node))
+                    extracted_functions.append(
+                        {
+                            "name": fn_name_parser(node),
+                            "start_line": node.start_point[0] + 1,
+                            "end_line": node.end_point[0] + 1,
+                        }
+                    )
+                functions[path] = extracted_functions
+            repo["functions"] = functions
+
+    # update the dataset
+    with open(dataset, "w") as f_out:
+        json.dump(lists, f_out)
+
+
+if __name__ == "__main__":
+    from fire import Fire
+
+    Fire(main)
