0dde4cb95d717e7b6377a6cd31972e2015c0dee9
Jiale Tom Tian <47706631+JialeTomTian@users.noreply.github.com>
2024-04-15 00:21:00 -0500

    feat: function similarity (#21)

---
+++ repoqa/function_similarity.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: (c) 2024 EvalPlus Team
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import re
+
+from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
+
+
+def compute_function_similarity(
+    candidate_function: str, reference_function: str
+) -> float:
+    candidate_tokens = [item for item in re.split("\s+", candidate_function.strip())]
+
+    reference_tokens = [item for item in re.split("\s+", reference_function.strip())]
+
+    chencherry = SmoothingFunction()
+
+    return sentence_bleu(
+        [reference_tokens], candidate_tokens, smoothing_function=chencherry.method7
+    )
