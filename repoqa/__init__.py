2f6e189ddb5b86cd559048ed0f6474825dee2092
ganler <jaway.liu@gmail.com>
2024-04-22 15:20:00 -0500

    feat: setup pypi release and fix the dependency

--- repoqa/__init__.py
+++ repoqa/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: (c) 2024 EvalPlus Team
 #
 # SPDX-License-Identifier: Apache-2.0
+
+try:
+    from repoqa._version import __version__, __version_tuple__
+except ImportError:
+    __version__ = "local-dev"
